b2119973a35721c453277d7a8605c7cf3bdb7217
ultrawazer <ultrawazer@hotmail.com>
2026-09-09 02:17:52 +0100

    make ExtensionContext generic with index signature

--- server/src/extensions/extension.types.ts
+++ server/src/extensions/extension.types.ts
@@ -130,9 +130,8 @@ export interface SearchOptions {
 export interface ExtensionContext {
   ua?: string
   cookie?: string
-  jasmr_ua?: string
-  jasmr_cookie?: string
   headers?: Record<string, string>
+  [key: string]: any
 }
 
 export interface IExtension {
